4687e9141d346656de945b41b5109877585472a4
dlrjswn2010 <leegeonju0710@gmail.com>
2026-03-18 15:18:58 +0900

    ch06 구구단

---
+++ ch06/ndan.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("단 수를 입력하세요. : ", function(dan) {
+    for (let i = 1; i <= 9; i++) {
+        console.log(dan * i);
+    }
+    rl.close();
+});
